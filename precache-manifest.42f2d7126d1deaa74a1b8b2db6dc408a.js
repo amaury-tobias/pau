7d1b0dc6c246036f469e51b36c1e8da3f286b8e7
amaury-tobias <amaury-tobias@users.noreply.github.com>
2020-06-17 21:02:46 +0000

    deploy: e3afa0cf445d3ab1682da239e09d7e6f1876f4ce

---
+++ precache-manifest.42f2d7126d1deaa74a1b8b2db6dc408a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0af0eebe64430f32b3c",
+    "url": "/css/app.9aecc725.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "1ee98ec6b2e638b761c07c8e7c9fedb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3deeb11e90f13b771834",
+    "url": "/js/about.e92b156f.js"
+  },
+  {
+    "revision": "a0af0eebe64430f32b3c",
+    "url": "/js/app.3f40f2b3.js"
+  },
+  {
+    "revision": "f9a7078613e7dd666888",
+    "url": "/js/chunk-vendors.0f4403ed.js"
+  },
+  {
+    "revision": "cd1437d80d6786f2e8edb01799465dfe",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
