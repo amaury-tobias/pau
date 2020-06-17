db7992d10f94c245193254b8093bcf81c2d30034
amaury-tobias <amaury.tobiasqr@gmail.com>
2020-06-17 18:02:24 -0500

    chore: release beta@0.0.1

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+import VueParticles from 'vue-particles'
+Vue.use(VueParticles)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
