0b4d02dcb216a46d8399072c49abfd045a19f189
amaury-tobias <amaury.tobiasqr@gmail.com>
2020-06-17 18:01:13 -0500

    feat: handle portrait/landscape mode on App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,73 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div v-if="!isPortrait" class="portrait">
+      <div class="portrait-container">
+        <p>Por favor coloca el dispositivo en modo retrato</p>
+        <p>💛❤🧡💛💚💙💜🤎🖤</p>
+      </div>
     </div>
     <router-view />
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data: () => ({
+    isPortrait: true
+  }),
+  mounted() {
+    this.handleOrientationChange()
+    window.addEventListener('orientationchange', this.handleOrientationChange)
+  },
+  methods: {
+    handleOrientationChange() {
+      const orientation = window.orientation
+      if (Math.abs(Number(orientation)) === 90) this.isPortrait = false
+      else this.isPortrait = true
+    }
+  }
+})
+</script>
+
 <style>
+html {
+  background-color: #000;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  height: 100%;
   text-align: center;
   color: #2c3e50;
+  overflow: hidden;
 }
 
-#nav {
-  padding: 30px;
+body {
+  margin: 0;
+  height: 100vh;
+  widows: 100vw;
+  overflow: hidden;
+  position: relative;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.portrait {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: #000;
+  z-index: 9999;
+  color: #fff;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.portrait-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
