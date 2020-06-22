c4917cfaf9c3e9ca94ec25d4c3f70ab9c8de954a
amaury-tobias <amaury.tobiasqr@gmail.com>
2020-06-22 16:33:31 -0500

    refactor: update Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <router-link to="music">Music</router-link>
+    <router-link to="music">
+      <div>
+        <img alt="Yellow Love" src="../assets/logo.png" />
+        <p>MUSIC</p>
+      </div>
+    </router-link>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
+<script lang="ts">
+import { defineComponent } from 'vue'
 
-export default {
+export default defineComponent({
   name: 'Home',
-}
+})
 </script>
+
+<style scoped>
+.home {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+}
+img {
+  transition: all 0.3s;
+  border-radius: 2rem;
+  width: 20%;
+  object-fit: contain;
+}
+img:hover {
+  width: 25%;
+}
+a {
+  display: block;
+  text-decoration: none;
+  color: #ffff80;
+  font-weight: 800;
+  margin-top: 1rem;
+}
+a:focus {
+  outline: none;
+}
+</style>
